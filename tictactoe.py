d04832cb37dcd972b8dffa7415e98241bef7646c
Emily Wang <emily.wang@students.olin.edu>
2013-04-07 23:09:28 -0400

    board dictionary version works now

--- tictactoe.py
+++ tictactoe.py
@@ -3,11 +3,11 @@
 #tictactoe
 
 def play(board, you, move):
-	if move in tictactoe_dict['x'] or tictactoe_dict['o']:	
+	if move in board['x'] or move in board['o']:	
 		print ("Hey, this spot is already occupied!")
 		return False
 	else:
-		tictactoe_dict[you].append(move)
+		board[you].append(move)
 	return board
 
 def visualize_board (board):
@@ -25,73 +25,64 @@ def game_over(board):
 	#did you win? 
 	#also, is there a more concise way to do this?
 	
-	#horizontal victory
-	if board[1] == board[2] == board[3]:
-		return True
-	if board[4] == board[5] == board[6]: 
-		return True
-	if board[7] == board[8] == board[9]: 
-		return True
-
-	#vertical victory
-	if board[1] == board[4] == board[7]: 
-		return True
-	if board[2] == board[5] == board[8]: 
-		return True
-	if board[3] == board[6] == board[9]: 
-		return True
-
-	#diagonal victory
-	if board[1] == board[5] == board[9]: 
-		return True
-	if board[3] == board[5] == board[7]: 
-		return True		
-
-	#nobody has won yet	
-	return False 
-
-def tictactoe():
-
-	#initial values :D
-	#board = [None, 1, 2, 3, 4, 5, 6, 7, 8, 9] 
-	board = {'x':[], 'o':[]}
-	you = 'x' #x goes first!
-
-	welcome = """
-				Hi there. Let's play tic tac toe! 
-				Here's a board; tell me where you want to go. 
-				1 2 3
-				4 5 6
-				7 8 9	
-				"""
-	print (welcome)
-
-	while game_over(board) == False: 
-		move = int(input("Player of " + you + ", make your move. "))
-
-		#python's doing it to the board
-		newboard = play (board, you, move)
-		while newboard == False:
-			move = int(input("Player of " + you + ", try again elsewhere. "))
-			newboard = play(board, you, move)
+	for you in board: #you is either the X player or O player
 		
-		#player made a move; now show them the new board
-		print(visualize_board(board))
+		#horizontal victory
+		if 1 in board[you] and 2 in board[you] and 3 in board[you]:
+			return str(you) + " is victorious."
+		if 4 in board[you] and 5 in board[you] and 6 in board[you]:
+			return str(you) + " is victorious."
+		if 7 in board[you] and 8 in board[you] and 9 in board[you]:
+			return str(you) + " is victorious."
 		
-		#next player's turn!
-		if you == 'x':
-			you = 'o'
-		else: 
-			you = 'x'
+		#vertical victory
+		if 1 in board[you] and 4 in board[you] and 7 in board[you]:
+			return str(you) + " is victorious."
+		if 2 in board[you] and 5 in board[you] and 8 in board[you]:
+			return str(you) + " is victorious."
+		if 3 in board[you] and 6 in board[you] and 9 in board[you]:
+			return str(you) + " is victorious."
+		
+		#diagonal victory
+		if 1 in board[you] and 5 in board[you] and 9 in board[you]:
+			return str(you) + " is victorious."
+		if 3 in board[you] and 5 in board[you] and 7 in board[you]:
+			return str(you) + " is victorious."
+	
+	if len(board['x']) + len(board['o']) == 9:
+		return "It's a tie."
+	else: #nobody has won yet
+		return False
 
-		#setting up the board for the next turn. 	
-		board = newboard 
+def tictactoe(player1x,player2o):
+	# initial values
+	if player1x == 'human' and player2o == 'human':
+		board = {'x':[], 'o':[]}
+		you = 'x' #x goes first!
+		welcome = """
+					Hi there. Let's play tic tac toe! 
+					Here's a board; tell me where you want to go. 
+					1 2 3
+					4 5 6
+					7 8 9	
+					"""
+		print (welcome)
+		while game_over(board) == False: 
+			move = int(input("Player of " + you + ", make your move. "))
+			#python's doing it to the board
+			newboard = play (board, you, move)
+			while newboard == False:
+				move = int(input("Player of " + you + ", try again elsewhere. "))
+				newboard = play(board, you, move)
+			#player made a move; now show them the new board
+			print(visualize_board(board))
+			#next player's turn!
+			if you == 'x':
+				you = 'o'
+			else: 
+				you = 'x'
+			#setting up the board for the next turn. 	
+			board = newboard 
+		print (game_over(board))
 
-	if you =='x': #because right now you == the loser
-		the_end = "Congratulations! Player o is victorious."
-	else: 
-		the_end = "Congratulations! Player x is victorious."
-
-	print (the_end)
-
-tictactoe()
+tictactoe('human', 'human')
